25a9f33c40479f8ccfa963c3bf9dbd3bdc087963
3119005159肖扬 <1173111391@qq.com>
2021-10-26 00:00:45 +0800

    Update routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,10 @@
 import Vapor
 
+
+struct Greeting: Content {
+    var hello: String
+}
+
 func routes(_ app: Application) throws {
     app.get { req in
         return "It works!"
@@ -7,5 +12,21 @@ func routes(_ app: Application) throws {
 
     app.get("hello") { req -> String in
         return "Hello, world!"
+    }
+    app.get("hello", "vapor") { req in
+        return "Hello, vapor!"
+    }
+    app.on(.GET, "hello", "vapor") { req in
+        return "Hello vapor!"
+    }
+    app.get("hello", ":name") { req -> String in
+        let name = req.parameters.get("name")!
+        return "Hello, \(name)!"
+    }
+    app.post("greeting") { req -> HTTPStatus in
+        let greeting = try req.content.decode(Greeting.self)
+        print(greeting.hello)
+        return HTTPStatus.ok
+        
     }
 }
